225e1577b3cb6b2dee154d9f1b773c63ddbc9adc
muddymudskipper <thomas.wilmering@gmail.com>
2025-03-07 16:38:07 +0000

    edit

--- cmem_plugin_splitfile/__init__.py
+++ cmem_plugin_splitfile/__init__.py
@@ -8,7 +8,7 @@ CONF = Path("/opt/cmem/eccenca-DataIntegration/dist/etc/dataintegration/conf/dat
 PROJECTFILEPATH = ""
 if CONF.is_file():
     with CONF.open("r") as conf_file:
-        conf_lines = list(conf_file.readlines())
+        conf_lines = conf_file.readlines()
     for line, text in enumerate(conf_lines):
         if text == "workspace.repository.projectFile = {\n":
             match = re.search(r'=\s*["\'](.*?)["\']', conf_lines[line + 1])

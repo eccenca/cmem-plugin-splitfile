318e6f79895b099958c04cdea168c6a09b60b583
Thomas Wilmering <thomas.wilmering@eccenca.com>
2025-08-18 13:16:32 +0100

    remove manifest file writing from groupedPrefix

--- cmem_plugin_splitfile/split_by_grouped_prefix.py
+++ cmem_plugin_splitfile/split_by_grouped_prefix.py
@@ -1,6 +1,5 @@
 """Split grouped prefix"""
 
-import csv
 import logging
 import ntpath
 import time
@@ -37,7 +36,6 @@ class SplitGroupedPrefix:
         self._outputdir = outputdir
         self._splitdelimiter = SPLIT_DELIMITER
         self._splitzerofill = ZERO_FILL
-        self._manfilename = MANIFEST_FILE_NAME
         self._starttime = time.time()
 
     @property
@@ -81,23 +79,11 @@ class SplitGroupedPrefix:
             )
         self._splitzerofill = value
 
-    @property
-    def manfilename(self) -> str:
-        """Return manifest filename"""
-        return self._manfilename
-
-    @manfilename.setter
-    def manfilename(self, value: str) -> None:
-        self._manfilename = value
-
     def _getnextsplit(self, splitnum: int) -> str:
         filename = ntpath.split(self.inputfile)[1]
         fname, ext = ntpath.splitext(filename)
         zsplitnum = format(splitnum, "0" + str(self.splitzerofill))
         return f"{fname}{self.splitdelimiter}{zsplitnum}{ext}"
-
-    def _getmanifestpath(self) -> str:
-        return str(Path(self.outputdir) / self.manfilename)
 
     def _endprocess(self) -> None:
         endtime = time.time()
@@ -108,91 +94,81 @@ class SplitGroupedPrefix:
         self, maxsize: int, callback: Callable[[str, int], Any] | None = None
     ) -> None:
         """Streaming split, keeping groups intact, packing multiple groups per file if possible"""
-        manifest_path = self._getmanifestpath()
-        with Path(manifest_path).open("w+", encoding="utf8", newline="") as writer:
-            fieldnames = ["filename", "filesize", "header"]
-            manifest = csv.DictWriter(writer, fieldnames=fieldnames, quoting=csv.QUOTE_MINIMAL)
-            manifest.writeheader()
+        splitnum = 1
+        current_prefix = None
+        current_size = 0
+        group_size = 0
+        outfile = None
+        outfile_path = None
 
-            splitnum = 1
-            current_prefix = None
-            current_size = 0
-            group_size = 0
-            outfile = None
-            outfile_path = None
+        def close_outfile() -> None:
+            nonlocal outfile, outfile_path, current_size
+            if outfile:
+                outfile.close()
+                size = Path(outfile_path).stat().st_size
+                if callback:
+                    callback(str(outfile_path), size)
+                outfile = None
+                current_size = 0
 
-            def close_outfile() -> None:
-                nonlocal outfile, outfile_path, current_size
-                if outfile:
-                    outfile.close()
-                    size = Path(outfile_path).stat().st_size
-                    manifest.writerow(
-                        {"filename": outfile_path.name, "filesize": size, "header": False}
-                    )
-                    if callback:
-                        callback(str(outfile_path), size)
-                    outfile = None
-                    current_size = 0
+        with Path(self.inputfile).open("rb") as infile:
+            for line in infile:
+                if self.terminate:
+                    log.info("Terminate flag set. Stopping split.")
+                    break
 
-            with Path(self.inputfile).open("rb") as infile:
-                for line in infile:
-                    if self.terminate:
-                        log.info("Terminate flag set. Stopping split.")
-                        break
+                if not line.strip():
+                    continue
 
-                    if not line.strip():
-                        continue
+                prefix = line.split(None, 1)[0]
 
-                    prefix = line.split(None, 1)[0]
+                # First line
+                if current_prefix is None:
+                    current_prefix = prefix
+                    group_size = 0
+                    if outfile is None:
+                        outfile_path = Path(self.outputdir) / self._getnextsplit(splitnum)
+                        outfile = outfile_path.open("wb")
 
-                    # First line
-                    if current_prefix is None:
-                        current_prefix = prefix
-                        group_size = 0
-                        if outfile is None:
-                            outfile_path = Path(self.outputdir) / self._getnextsplit(splitnum)
-                            outfile = outfile_path.open("wb")
+                # New prefix -> check group size & possible file change
+                if prefix != current_prefix:
+                    if group_size > maxsize:
+                        close_outfile()
+                        raise ValueError(
+                            f'Group with prefix "{current_prefix.decode(errors="ignore")}" '
+                            f"exceeds max file size."
+                        )
 
-                    # New prefix -> check group size & possible file change
-                    if prefix != current_prefix:
-                        if group_size > maxsize:
-                            close_outfile()
-                            raise ValueError(
-                                f'Group with prefix "{current_prefix.decode(errors="ignore")}" '
-                                f"exceeds max file size."
-                            )
-
-                        # If adding this group would exceed file size, start new file
-                        if current_size + group_size > maxsize:
-                            close_outfile()
-                            splitnum += 1
-                            outfile_path = Path(self.outputdir) / self._getnextsplit(splitnum)
-                            outfile = outfile_path.open("wb")
-
-                        current_prefix = prefix
-                        group_size = 0
-
-                    # Accumulate size for the current group
-                    group_size += len(line)
-
-                    # If writing current line would overflow the file, split within same group
-                    if current_size + len(line) > maxsize:
+                    # If adding this group would exceed file size, start new file
+                    if current_size + group_size > maxsize:
                         close_outfile()
                         splitnum += 1
                         outfile_path = Path(self.outputdir) / self._getnextsplit(splitnum)
                         outfile = outfile_path.open("wb")
 
-                    outfile.write(line)  # type: ignore[union-attr]
-                    current_size += len(line)
+                    current_prefix = prefix
+                    group_size = 0
 
-                # Final group size check
-                if group_size > maxsize:
+                # Accumulate size for the current group
+                group_size += len(line)
+
+                # If writing current line would overflow the file, split within same group
+                if current_size + len(line) > maxsize:
                     close_outfile()
-                    raise ValueError(
-                        f'Group with prefix "{current_prefix.decode(errors="ignore")}" '  # type: ignore[union-attr]
-                        f"exceeds max file size."
-                    )
+                    splitnum += 1
+                    outfile_path = Path(self.outputdir) / self._getnextsplit(splitnum)
+                    outfile = outfile_path.open("wb")
 
-            close_outfile()
+                outfile.write(line)  # type: ignore[union-attr]
+                current_size += len(line)
 
+            # Final group size check
+            if group_size > maxsize:
+                close_outfile()
+                raise ValueError(
+                    f'Group with prefix "{current_prefix.decode(errors="ignore")}" '  # type: ignore[union-attr]
+                    f"exceeds max file size."
+                )
+
+        close_outfile()
         self._endprocess()
